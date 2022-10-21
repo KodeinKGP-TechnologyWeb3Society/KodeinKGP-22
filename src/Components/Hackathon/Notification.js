2e196b2f6240fa5f7f3379f8b07ab1c2215317c9
Suraj Gupta <82136372+surajgupta3072@users.noreply.github.com>
2022-10-21 21:13:37 +0530

    Added titles

--- src/Components/Hackathon/Notification.js
+++ src/Components/Hackathon/Notification.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Notification = () => {
   return (
     <div>
-      Notification
+      <div className="heading_box" style={{ marginBottom: "10%" }}>
+        <div>
+          <p class="btn">
+            <span>
+              <span>
+                <span className="border_box">Notifications</span>
+              </span>
+            </span>
+          </p>
+        </div>
+        <div style={{ marginTop: "70px" }}>
+          <h4 className="page3_subtitle text3">Stay Updated with any announcements</h4>
+        </div>
+      </div>
     </div>
   )
 }
