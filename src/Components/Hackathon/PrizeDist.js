093b512861050b98018574409c14324da8aa5327
Aritra Chakraborty <aritracodm2002@kgpian.iitkgp.ac.in>
2022-10-21 23:28:39 +0530

    Modified PrizeDist.js

--- src/Components/Hackathon/PrizeDist.js
+++ src/Components/Hackathon/PrizeDist.js
@@ -13,6 +13,439 @@ const PrizeDist = () => {
             </span>
           </p>
         </div>
+        <app-prizes className="ng-star-inserted">
+          <div className="d-flex d_flex ng-star-inserted">
+            <div className="row">
+              <div className="item ng-star-inserted">
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">
+                    {" "}
+                    Winner{" "}
+                  </h3>
+                  {/**/}
+                  {/**/}
+                  {/**/}
+                </div>
+                <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                  <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                    <em
+                      _ngcontent-d2c-frontend-c160=""
+                      aria-hidden="true"
+                      className="fa fa-rupee"
+                    />{" "}
+                    25,000{" "}
+                  </strong>
+                </div>
+                {/**/}
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon ng-star-inserted"
+                />
+                {/**/}
+                {/**/}
+                <div
+                  _ngcontent-d2c-frontend-c160=""
+                  className="certificate ng-star-inserted"
+                >
+                  {" "}
+                  Certificate{" "}
+                </div>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              {/**/}
+            <div className="item ng-star-inserted">
+              <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                <h3 _ngcontent-d2c-frontend-c160="" className=" heading ng-star-inserted">
+                  {" "}
+                  First Runner Up{" "}
+                </h3>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                  <em
+                    _ngcontent-d2c-frontend-c160=""
+                    aria-hidden="true"
+                    className="fa fa-rupee"
+                  />{" "}
+                  20,000{" "}
+                </strong>
+              </div>
+              {/**/}
+              <img
+                _ngcontent-d2c-frontend-c160=""
+                src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                alt="Trophy Icon"
+                width={78}
+                height={78}
+                className="trophy_icon ng-star-inserted"
+              />
+              {/**/}
+              {/**/}
+              <div
+                _ngcontent-d2c-frontend-c160=""
+                className="certificate ng-star-inserted"
+              >
+                {" "}
+                Certificate{" "}
+              </div>
+              {/**/}
+              {/**/}
+              {/**/}
+            </div>
+            <div className="item ng-star-inserted last">
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">
+                    {" "}
+                    Second Runner Up{" "}
+                  </h3>
+                  {/**/}
+                  {/**/}
+                  {/**/}
+                </div>
+                <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                  <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                    <em
+                      _ngcontent-d2c-frontend-c160=""
+                      aria-hidden="true"
+                      className="fa fa-rupee"
+                    />{" "}
+                    15,000{" "}
+                  </strong>
+                </div>
+                {/**/}
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon ng-star-inserted"
+                />
+                {/**/}
+                {/**/}
+                <div
+                  _ngcontent-d2c-frontend-c160=""
+                  className="certificate ng-star-inserted"
+                >
+                  {" "}
+                  Certificate{" "}
+                </div>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              {/**/}
+              {/**/}
+            {/**/}
+            </div>
+            <div className="row">
+              <div className="item ng-star-inserted">
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">
+                    {" "}
+                    Winner{" "}
+                  </h3>
+                  {/**/}
+                  {/**/}
+                  {/**/}
+                </div>
+                <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                  <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                    <em
+                      _ngcontent-d2c-frontend-c160=""
+                      aria-hidden="true"
+                      className="fa fa-rupee"
+                    />{" "}
+                    25,000{" "}
+                  </strong>
+                </div>
+                {/**/}
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon ng-star-inserted"
+                />
+                {/**/}
+                {/**/}
+                <div
+                  _ngcontent-d2c-frontend-c160=""
+                  className="certificate ng-star-inserted"
+                >
+                  {" "}
+                  Certificate{" "}
+                </div>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              {/**/}
+            <div className="item ng-star-inserted">
+              <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                <h3 _ngcontent-d2c-frontend-c160="" className=" heading ng-star-inserted">
+                  {" "}
+                  First Runner Up{" "}
+                </h3>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                  <em
+                    _ngcontent-d2c-frontend-c160=""
+                    aria-hidden="true"
+                    className="fa fa-rupee"
+                  />{" "}
+                  20,000{" "}
+                </strong>
+              </div>
+              {/**/}
+              <img
+                _ngcontent-d2c-frontend-c160=""
+                src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                alt="Trophy Icon"
+                width={78}
+                height={78}
+                className="trophy_icon ng-star-inserted"
+              />
+              {/**/}
+              {/**/}
+              <div
+                _ngcontent-d2c-frontend-c160=""
+                className="certificate ng-star-inserted"
+              >
+                {" "}
+                Certificate{" "}
+              </div>
+              {/**/}
+              {/**/}
+              {/**/}
+            </div>
+            <div className="item ng-star-inserted last">
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">
+                    {" "}
+                    Second Runner Up{" "}
+                  </h3>
+                  {/**/}
+                  {/**/}
+                  {/**/}
+                </div>
+                <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                  <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                    <em
+                      _ngcontent-d2c-frontend-c160=""
+                      aria-hidden="true"
+                      className="fa fa-rupee"
+                    />{" "}
+                    15,000{" "}
+                  </strong>
+                </div>
+                {/**/}
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon ng-star-inserted"
+                />
+                {/**/}
+                {/**/}
+                <div
+                  _ngcontent-d2c-frontend-c160=""
+                  className="certificate ng-star-inserted"
+                >
+                  {" "}
+                  Certificate{" "}
+                </div>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              {/**/}
+              {/**/}
+            {/**/}
+            </div>
+            <div className="row">
+              <div className="item ng-star-inserted">
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">
+                    {" "}
+                    Winner{" "}
+                  </h3>
+                  {/**/}
+                  {/**/}
+                  {/**/}
+                </div>
+                <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                  <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                    <em
+                      _ngcontent-d2c-frontend-c160=""
+                      aria-hidden="true"
+                      className="fa fa-rupee"
+                    />{" "}
+                    25,000{" "}
+                  </strong>
+                </div>
+                {/**/}
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon ng-star-inserted"
+                />
+                {/**/}
+                {/**/}
+                <div
+                  _ngcontent-d2c-frontend-c160=""
+                  className="certificate ng-star-inserted"
+                >
+                  {" "}
+                  Certificate{" "}
+                </div>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              {/**/}
+            <div className="item ng-star-inserted">
+              <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                <h3 _ngcontent-d2c-frontend-c160="" className=" heading ng-star-inserted">
+                  {" "}
+                  First Runner Up{" "}
+                </h3>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                  <em
+                    _ngcontent-d2c-frontend-c160=""
+                    aria-hidden="true"
+                    className="fa fa-rupee"
+                  />{" "}
+                  20,000{" "}
+                </strong>
+              </div>
+              {/**/}
+              <img
+                _ngcontent-d2c-frontend-c160=""
+                src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                alt="Trophy Icon"
+                width={78}
+                height={78}
+                className="trophy_icon ng-star-inserted"
+              />
+              {/**/}
+              {/**/}
+              <div
+                _ngcontent-d2c-frontend-c160=""
+                className="certificate ng-star-inserted"
+              >
+                {" "}
+                Certificate{" "}
+              </div>
+              {/**/}
+              {/**/}
+              {/**/}
+            </div>
+            <div className="item ng-star-inserted last">
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">
+                    {" "}
+                    Second Runner Up{" "}
+                  </h3>
+                  {/**/}
+                  {/**/}
+                  {/**/}
+                </div>
+                <div _ngcontent-d2c-frontend-c160="" className="trophy ng-star-inserted">
+                  <strong _ngcontent-d2c-frontend-c160="" className="blue-text">
+                    <em
+                      _ngcontent-d2c-frontend-c160=""
+                      aria-hidden="true"
+                      className="fa fa-rupee"
+                    />{" "}
+                    15,000{" "}
+                  </strong>
+                </div>
+                {/**/}
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon ng-star-inserted"
+                />
+                {/**/}
+                {/**/}
+                <div
+                  _ngcontent-d2c-frontend-c160=""
+                  className="certificate ng-star-inserted"
+                >
+                  {" "}
+                  Certificate{" "}
+                </div>
+                {/**/}
+                {/**/}
+                {/**/}
+              </div>
+              {/**/}
+              {/**/}
+            {/**/}
+            </div>
+            <div className="row">
+              <div
+                _ngcontent-d2c-frontend-c160=""
+                className="item no_prize ng-star-inserted"
+              >
+                <div _ngcontent-d2c-frontend-c160="" className="full-width">
+                  <h3 _ngcontent-d2c-frontend-c160="" className="heading ng-star-inserted">Participation Certificate</h3>
+                </div>
+                <img
+                  _ngcontent-d2c-frontend-c160=""
+                  src="https://d8it4huxumps7.cloudfront.net/uploads/images/svg-images/unstop-trophy.png"
+                  alt="Trophy Icon"
+                  width={78}
+                  height={78}
+                  className="trophy_icon"
+                />
+                <div _ngcontent-d2c-frontend-c160="" className="certificate">
+                  {" "}
+                  Certificate{" "}
+                </div>
+              </div>
+              {/**/}
+            </div>
+            <div
+              className="blue-text prize-date ng-star-inserted"
+            >
+              {" "}
+              * All the prizes and certificate will be Released Within 7 days after the
+              event is over{" "}
+            </div>
+            {/**/}
+            {/**/}
+            {/**/}
+          </div>
+        </app-prizes>
         <div style={{ marginTop: "70px" }}>
           <h4 className="page3_subtitle text3">Prize worth 2 Lakhs!!</h4>
         </div>
