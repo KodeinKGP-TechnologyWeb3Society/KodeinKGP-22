ff0a8a7b993dc022eafccdca6161018d0cdf4e25
Sumit Kumar Sharma <sksharma372@kgpian.iitkgp.ac.in>
2022-10-22 18:16:55 +0530

    notifications and rules updated

--- src/Components/Hackathon/Hackathon.js
+++ src/Components/Hackathon/Hackathon.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { ReactNotifications, Store } from 'react-notifications-component';
+import 'animate.css'
+import 'react-notifications-component/dist/theme.css'
+
 import Notification from './Notification'
 import PrizeDist from './PrizeDist'
 import Rules from './Rules'
--- src/Components/Hackathon/Notification.js
+++ src/Components/Hackathon/Notification.js
@@ -1,6 +1,24 @@
 import React from 'react'
+import { ToastContainer, toast, Slide } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Notification = () => {
+  // const toastId = React.useRef(null);
+  // const toastId2 = React.useRef(null);
+  // const toastId3 = React.useRef(null);
+
+  // const notify = () => {
+  //   if (! toast.isActive(toastId.current)){
+  //    toastId.current = toast.success("this is first", {theme: 'dark', transition: Slide, autoClose: false, className: 'toast1'});
+  //   }
+  //   if (! toast.isActive(toastId2.current)){
+  //     toastId2.current = toast.success('this is second', {theme: 'dark', draggable: true, transition: Slide, autoClose: false,position: toast.POSITION.TOP_CENTER});
+  //   }
+  //   if (! toast.isActive(toastId3.current)){
+  //     toastId3.current = toast.success('this is third', {theme: 'dark', draggable: true, transition: Slide, autoClose: false,position: toast.POSITION.TOP_CENTER});
+  //   }
+  // }
   return (
     <div>
       <div className="heading_box" style={{ marginBottom: "10%" }}>
@@ -13,8 +31,18 @@ const Notification = () => {
             </span>
           </p>
         </div>
+        <div className="notifications_content">
+          {/* <ToastContainer /> */}
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere commodi quae animi neque beatae amet.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere commodi quae animi neque beatae amet.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere commodi quae animi neque beatae amet.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere commodi quae animi neque beatae amet.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere commodi quae animi neque beatae amet.</li>
+          </ul>
+        </div>
         <div style={{ marginTop: "70px" }}>
-          <h4 className="page3_subtitle text3">Stay Updated with any announcements</h4>
+          <h4 style={{width: '100%', textAlign: 'center'}} className="page3_subtitle text3">-----Stay Updated with any announcements-----</h4>
         </div>
       </div>
     </div>
