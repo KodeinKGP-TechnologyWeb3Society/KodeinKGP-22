6f28c895141406d4bd0a27e1586d98c569dfa51e
Rajarshi Mandal <rajarshimandal170802@gmail.com>
2023-01-23 16:40:42 +0530

    work till 23.01.22 16:40

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -12,11 +12,10 @@ export default function Events() {
                             <div className="card shadow-sm">
                                 <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                                 <div className="card-body">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text" style={{color: "black", textAlign: "center"}}><strong>Tile Should be put here</strong></p>
                                     <div className="d-flex justify-content-between align-items-center">
                                         <div className="btn-group">
                                             <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
                                         </div>
                                         
                                     </div>
@@ -27,11 +26,10 @@ export default function Events() {
                             <div className="card shadow-sm">
                                 <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                                 <div className="card-body">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text" style={{color: "black", textAlign: "center"}}><strong>Tile Should be put here</strong></p>
                                     <div className="d-flex justify-content-between align-items-center">
                                         <div className="btn-group">
                                             <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
                                         </div>
                                         
                                     </div>
@@ -42,11 +40,10 @@ export default function Events() {
                             <div className="card shadow-sm">
                                 <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                                 <div className="card-body">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text" style={{color: "black", textAlign: "center"}}><strong>Tile Should be put here</strong></p>
                                     <div className="d-flex justify-content-between align-items-center">
                                         <div className="btn-group">
                                             <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
                                         </div>
                                         
                                     </div>
