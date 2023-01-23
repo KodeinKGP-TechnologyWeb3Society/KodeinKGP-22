814c07881913bfc3815c02e0d572421d6794a102
Rajarshi Mandal <rajarshimandal170802@gmail.com>
2023-01-23 16:16:17 +0530

    work till 22.01.22

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Events.css';
+import bg from '../bg/bg.gif'
 
 export default function Events() {
     return (
         <div>
             <div className="album py-5 bg-light">
                 <div className="container">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" style={{backgroundImage: {bg}}}>
                         <div className="col">
                             <div className="card shadow-sm">
                                 <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
